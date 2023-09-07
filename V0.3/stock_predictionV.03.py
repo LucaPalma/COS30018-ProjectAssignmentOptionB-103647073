a3302956bff0310e185f849f455489feb500ec54
MSI\Luca <103647073@student.swin.edu.au>
2023-09-07 20:17:45 +1000

    Fixed Task 3 BoxPlot

--- V0.3/stock_predictionV.03.py
+++ V0.3/stock_predictionV.03.py
@@ -19,6 +19,7 @@ import tensorflow as tf
 import yfinance as yf
 import os
 import os.path
+import pickle
 
 from tensorflow import keras
 from sklearn.preprocessing import MinMaxScaler
@@ -35,8 +36,8 @@ PRICE_VALUE_UPPER = "Close"
 PRICE_VALUE_LOWER = "Open"
 
 # Display Parameters
-displayType = "graph"  # Can be set to graph, candle or boxplot
-displayTradingDays = 5  # from task B.3 select n trading days for chart to display.
+displayType = "boxplot"  # Can be set to graph, candle or boxplot
+displayTradingDays = 25  # from task B.3 select n trading days for chart to display.
 
 # Data loading and processing Parameters
 startdate = '2022-01-01'
@@ -48,11 +49,14 @@ scaleMin = 0
 scaleMax = 1
 
 # Model building parameters
-LSTM_Units = 50
+Cell_Units = 50
 Dense_Units = 1
 Dropout_Amt = 0.2
 Epoch_Count = 25
 Batch_Count = 32
+Layer_Name = "DeepLearningLayer"
+modelType = LSTM
+Layer_Number = 2
 
 
 def load_process_data(TRAIN_START, TRAIN_END, date_split=True, split_ratio=0.3, store_data=True,
@@ -72,6 +76,10 @@ def load_process_data(TRAIN_START, TRAIN_END, date_split=True, split_ratio=0.3, 
     scaler = MinMaxScaler(feature_range=(scale_min, scale_max))
     scaled_data = scaler.fit_transform(
         (data[PRICE_VALUE_UPPER].values.reshape(-1, 1) + data[PRICE_VALUE_UPPER].values.reshape(-1, 1)) / 2)
+
+    with open('minmax_scaler.pkl', 'wb') as f:
+        pickle.dump(scaled_data, f)
+    # Save the scaler to file.
 
     X, y = [], []
     for x in range(PREDICTION_DAYS, len(scaled_data)):
@@ -97,20 +105,56 @@ def load_process_data(TRAIN_START, TRAIN_END, date_split=True, split_ratio=0.3, 
     if store_data:
         data.to_csv("datafile")
 
-    model = build_model(x_train, y_train, LSTM_Units, Dense_Units, Dropout_Amt, Epoch_Count, Batch_Count)
+    # model = build_model(x_train, y_train, Cell_Units, Dense_Units, Dropout_Amt, Epoch_Count, Batch_Count, Layer_Name)
+
+    model = build_customModel(x_train, y_train, Cell_Units, Dense_Units, Dropout_Amt,
+                              Epoch_Count, Batch_Count, Layer_Name, modelType, Layer_Number)
+
     load_test_data(TRAIN_START, TRAIN_END, COMPANY, data, model, scaler, PREDICTION_DAYS,
                    displayType)
 
 
-def build_model(x_train, y_train, unitsLSTM, unitsDense, dropOutAmt, epochAmt, batchAmt):
+def build_model(x_train, y_train, unitsLSTM, unitsDense, dropOutAmt, epochAmt, batchAmt, layerName):
     model = Sequential()  # Basic neural network
     model.add(LSTM(units=unitsLSTM, return_sequences=True, input_shape=(x_train.shape[1], 1)))
     model.add(Dropout(dropOutAmt))
+
     model.add(LSTM(units=unitsLSTM, return_sequences=True))
     model.add(Dropout(dropOutAmt))
+
     model.add(LSTM(units=unitsLSTM))
     model.add(Dropout(dropOutAmt))
+
     model.add(Dense(units=unitsDense))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, epochs=epochAmt, batch_size=batchAmt)
+
+    # Update layer names
+    for layer in model.layers:
+        layer._name = layer.name + layerName
+    return model
+
+
+def build_customModel(x_train, y_train, unitsCell, unitsDense, dropOutAmt, epochAmt,
+                      batchAmt, layerName, cell, layerNumber):
+    model = Sequential()
+    for i in range(layerNumber):
+        if i == 0:
+            # Runs on the first layer of the model, uses trained data.
+            model.add(cell(units=unitsCell, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+        elif i == layerNumber - 1:
+            # Runs on the last layer of the model.
+            model.add(cell(units=unitsCell, return_sequences=False))
+        else:
+            # Hidden layers
+            model.add(cell(units=unitsCell, return_sequences=True))
+
+        # Add dropout following each layer
+        model.add(Dropout(dropOutAmt))
+
+    model.add(Dense(units=unitsDense))
+    # Add a Dense layer
     model.compile(optimizer='adam', loss='mean_squared_error')
     model.fit(x_train, y_train, epochs=epochAmt, batch_size=batchAmt)
     return model
@@ -211,18 +255,26 @@ def displayCandle(data, COMPANY):
 
 def displayBoxPlot(data, COMPANY):
     chart_small = data[-displayTradingDays:]  # set number of days to display
-    plt.bar(x=chart_small["Date"], height=chart_small["Close"], color="Blue")
-    # create bar chart with x-axis as dates, y is daily closing price.
 
-    # plt.tight_layout possibly deprecated
-    plt.xlabel("Date")
+    plt.figure(figsize=(10, 15))
+    xvalues = ["Open", "Close", "Low", "High"]
+    plot = plt.boxplot(data[xvalues], patch_artist=True)
+    # Open, Close, Low, High, Volume
+
+    plt.yticks(range(100, 500, 20), ["{} $".format(v) for v in range(100, 500, 20)])
+    plt.xticks((1, 2, 3, 4), xvalues)
+
     plt.ylabel(f"{COMPANY} Share Price")
-    plt.title("Bar Chart")
+
+    plt.title(f"Boxplot Chart for last {displayTradingDays} days.")
     plt.subplots_adjust(bottom=0.25)  # Make sure dates don't get cutoff screen
     # update labels, layout and title
 
-    plt.xticks(rotation=75)
-    # rotate dates for easier reading.
+    colours = ['g', 'b', 'orange', 'r']
+    for patch, color in zip(plot['boxes'], colours):
+        patch.set_facecolor(color)
+
+    plt.grid(color='grey')
 
     plt.show()
 
